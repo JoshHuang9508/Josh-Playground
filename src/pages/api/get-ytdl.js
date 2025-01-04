78ae41244da2b4e8fe5fec2aa15e85a3d0d1f1d6
Whydog <142924636+JoshHuang9508@users.noreply.github.com>
2025-01-05 03:23:30 +0800

    嘗試修復無法取得影片資訊

--- src/pages/api/get-ytdl.js
+++ src/pages/api/get-ytdl.js
@@ -4,7 +4,15 @@ export default async function handler(req, res) {
   const { videoId } = req.query;
   try {
     const info = await ytdl.getBasicInfo(
-      `https://www.youtube.com/watch?v=${videoId}`
+      `https://www.youtube.com/watch?v=${videoId}`,
+      {
+        requestOptions: {
+          headers: {
+            "User-Agent":
+              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+          },
+        },
+      }
     );
     res.status(200).json(info.videoDetails);
   } catch (error) {
