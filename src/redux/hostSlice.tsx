22cce1ca70178a2dbc98d5a34bb727307c35121c
Josh <joshhuang9508@gmail.com>
2025-06-15 03:25:07 +0800

    feat: 新增環境變數範例檔案並更新 hostSlice 的初始狀態為環境變數

--- src/redux/hostSlice.tsx
+++ src/redux/hostSlice.tsx
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const hostSlice = createSlice({
   name: "hostSlice",
-  initialState: "https://5417-2001-df2-45c1-75-00-1.ngrok-free.app" as string,
+  initialState: process.env.SERVER_URL || ("http://localhost:3000" as string),
   reducers: {},
 });
 
